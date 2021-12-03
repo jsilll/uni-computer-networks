1139fe27255dd85364ebcbddde3f4e6cadb30159
João Silveira <joao.freixial.silveira@tecnico.ulisboa.pt>
2021-12-03 10:35:30 +0000

    updated main.c

--- server/src/main.c
+++ server/src/main.c
@@ -1,4 +1,15 @@
-int main()
+/* main.c */
+/* 0 copyright/licensing */
+/* 1 includes */
+/* 2 defines */
+/* 3 external declarations */
+/* 4 typedefs */
+/* 5 global variable declarations */
+/* 6 function prototypes */
+
+int main(int argc, char *argv[])
 {
-    return 1;
+    /* 7 command-line parsing */
 }
+
+/* 8 function declarations */
