71f6640094731813ca8abc7604ea26a4f0730329
João Silveira <joao.freixial.silveira@tecnico.ulisboa.pt>
2021-12-04 01:12:00 +0000

    argument parsing for client

--- client/src/main.c
+++ client/src/main.c
@@ -1,15 +1,81 @@
-/* main.c */
-/* 0 copyright/licensing */
-/* 1 includes */
-/* 2 defines */
-/* 3 external declarations */
-/* 4 typedefs */
-/* 5 global variable declarations */
-/* 6 function prototypes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define GROUP_NUMBER 6
+
+int PORT = 58000 + GROUP_NUMBER;
+int IP = 0;
+
+void parseArgs(int argc, char *argv[]);
+int validateIPArg(char *port);
+int validatePortArg(char *port);
 
 int main(int argc, char *argv[])
 {
-    /* 7 command-line parsing */
+    parseArgs(argc, argv);
+    printf("%d %d\n", IP, PORT);
 }
 
-/* 8 function declarations */
+void parseArgs(int argc, char *argv[])
+{
+    int opt;
+    while ((opt = getopt(argc, argv, ":n:p:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'n':
+            IP = validateIPArg(optarg);
+            break;
+        case 'p':
+            PORT = validatePortArg(optarg);
+            break;
+        case ':':
+            fprintf(stderr, "Missing value for ip (-n) or port (-p) option\n");
+            exit(1);
+            break;
+        case '?':
+            fprintf(stderr, "Unknown option: %c\n", optopt);
+            exit(1);
+            break;
+        }
+    }
+
+    for (; optind < argc; optind++)
+    {
+        fprintf(stderr, "Unecessary extra argument: %s\n", argv[optind]);
+        exit(1);
+    }
+}
+
+int validateIPArg(char *port)
+{
+    // TODO
+    return atoi(port);
+}
+
+int validatePortArg(char *port)
+{
+    int is_zero = 1, port_length = strlen(port), port_parsed;
+    for (int i = 0; i < port_length; i++)
+    {
+        if (port[i] != '0')
+        {
+            is_zero = 0;
+            break;
+        }
+    }
+
+    if (is_zero)
+        return 0;
+
+    port_parsed = atoi(port);
+    if (port_parsed > 0 && port_parsed <= 65535)
+        return port_parsed;
+    else
+    {
+        fprintf(stderr, "Invalid port value\n");
+        exit(1);
+    }
+}
--- server/src/main.c
+++ server/src/main.c
@@ -1,83 +1,15 @@
-/* main.c */
-/* 0 copyright/licensing */
-/* 1 includes */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/* 2 defines */
-#define GROUP_NUMBER 6
-/* 3 external declarations */
-/* 4 typedefs */
+#include <unistd.h>
 
-/* 5 global variable declarations */
+#define GROUP_NUMBER 6
+
 int PORT = 58000 + GROUP_NUMBER;
 int VERBOSE = 0;
 
-/* 6 function prototypes */
-void parseArgs(int argc, char *argv[])
-{
-    /* Too many arguments */
-    if (argc > 4)
-    {
-        fprintf(stderr, "Too many arguments\n");
-        return exit(1);
-    }
-
-    int seen_v = 0;
-    int seen_p = 0;
-
-    for (int i = 0; i < argc; i++)
-    {
-        if (strcmp(argv[i], "-v") == 0)
-        {
-            if (seen_v == 1)
-            {
-                fprintf(stderr, "Repeated -v argument\n");
-                return exit(1);
-            }
-            seen_v = 1;
-            VERBOSE = 1;
-        }
-        else if (strcmp(argv[i], "-p") == 0)
-        {
-            if (seen_p == 1)
-            {
-                fprintf(stderr, "Repeated -p argument\n");
-                return exit(1);
-            }
-
-            seen_p = 1;
-            if (i > argc - 2)
-            {
-                fprintf(stderr, "Missing port number\n");
-                return exit(1);
-            }
-
-            int PORT_ARG = -1;
-            if (strcmp(argv[i + 1], "0") == 0)
-            {
-                PORT = 0;
-            }
-            else
-            {
-                PORT_ARG = atoi(argv[i + 1]);
-                if (PORT_ARG == 0 || PORT_ARG > 65535 || PORT_ARG < 0)
-                {
-                    fprintf(stderr, "Port must be between 0 and 65535\n");
-                    return exit(1);
-                }
-
-                PORT = PORT_ARG;
-            }
-        }
-    }
-
-    if (seen_v + seen_p * 2 != argc - 1)
-    {
-        fprintf(stderr, "Uknown argument\n");
-        return exit(1);
-    }
-}
+void parseArgs(int argc, char *argv[]);
+int validatePortArg(char *port);
 
 int main(int argc, char *argv[])
 {
@@ -85,4 +17,58 @@ int main(int argc, char *argv[])
     printf("%d %d\n", PORT, VERBOSE);
 }
 
-/* 8 function declarations */
+void parseArgs(int argc, char *argv[])
+{
+    int opt;
+    while ((opt = getopt(argc, argv, ":vp:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'p':
+            PORT = validatePortArg(optarg);
+            break;
+        case 'v':
+            VERBOSE = 1;
+            break;
+        case ':':
+            fprintf(stderr, "Missing value for port (-p) option\n");
+            exit(1);
+            break;
+        case '?':
+            fprintf(stderr, "Unknown option: %c\n", optopt);
+            exit(1);
+            break;
+        }
+    }
+
+    for (; optind < argc; optind++)
+    {
+        fprintf(stderr, "Unecessary extra argument: %s\n", argv[optind]);
+        exit(1);
+    }
+}
+
+int validatePortArg(char *port)
+{
+    int is_zero = 1, port_length = strlen(port), port_parsed;
+    for (int i = 0; i < port_length; i++)
+    {
+        if (port[i] != '0')
+        {
+            is_zero = 0;
+            break;
+        }
+    }
+
+    if (is_zero)
+        return 0;
+
+    port_parsed = atoi(port);
+    if (port_parsed > 0 && port_parsed <= 65535)
+        return port_parsed;
+    else
+    {
+        fprintf(stderr, "Invalid port value\n");
+        exit(1);
+    }
+}
