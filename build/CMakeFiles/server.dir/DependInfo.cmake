8966afaef08033473403ff881a01987c8cc0cfb2
João Silveira <joao.freixial.silveira@tecnico.ulisboa.pt>
2022-01-02 15:54:01 +0000

    added build

---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/joao/Documents/ist/RC/rc-project/server/main.c" "CMakeFiles/server.dir/server/main.c.o" "gcc" "CMakeFiles/server.dir/server/main.c.o.d"
+  "/home/joao/Documents/ist/RC/rc-project/server/state/operations.c" "CMakeFiles/server.dir/server/state/operations.c.o" "gcc" "CMakeFiles/server.dir/server/state/operations.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/server/main.c.o"
+  "CMakeFiles/server.dir/server/main.c.o.d"
+  "CMakeFiles/server.dir/server/state/operations.c.o"
+  "CMakeFiles/server.dir/server/state/operations.c.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
